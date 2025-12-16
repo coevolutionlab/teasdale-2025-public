e5f394acd425e41985eca5604ac267881dd4ffd3
Dr. K.D. Murray <foss@kdmurray.id.au>
2025-12-16 11:27:55 +1100

    attributes

--- nlr_seqs/make.sh
+++ nlr_seqs/make.sh
@@ -1,1 +1,21 @@
-../.git/annex/objects/Zp/m3/SHA256E-s350--d594959df6d5c5f0ff5f2e9b3d12ba2b1bbd11e45ce5b480e9cfaa4560eb2f4f.sh/SHA256E-s350--d594959df6d5c5f0ff5f2e9b3d12ba2b1bbd11e45ce5b480e9cfaa4560eb2f4f.sh
+<../annotation/final/nlr_table.csv \
+    csvtk filter2 -f '${Curated_type}=="nlr"' |
+    csvtk cut -f ID |
+    tail -n +2 > nlr_genes.txt
+
+zcat ../annotation/final/all_accesssions.hodgepodgemerged.sort.gff3.gz |
+    grep -f nlr_genes.txt \
+    >nlr_annotation.gff3
+
+wc -l nlr_annotation.gff3
+
+gffread \
+    -y all_nlrs_cds.fa \
+    -w all_nlrs_mrna.fa -S  \
+    -g ../assemblies/output/all_accessions.fasta \
+    nlr_annotation.gff3
+
+bgzip -@12 -l 9 all_nlrs_cds.fa 
+samtools faidx  all_nlrs_cds.fa.gz
+bgzip -@12 -l 9 all_nlrs_mrna.fa 
+samtools faidx  all_nlrs_mrna.fa.gz
